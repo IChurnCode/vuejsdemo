ffed9f7a07320636d75e1e1537b4a8337f661fef
iswarya <rajagopal.iswarya@gmail.com>
2019-02-18 18:31:49 +0530

    vuejs through cli

---
+++ src/components/Posts.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th scope="col">User Id</th>
+        <th scope="col">Id</th>
+        <th scope="col">Title</th>
+        <th scope="col">Body</th>
+      </tr>
+      </thead>
+      <tbody>
+
+      <tr v-for="post in posts" :key="post.id">
+        <td scope="row">{{post.userId}}</td>
+        <td scope="row">{{post.id}}</td>
+        <td>{{post.title }}</td>
+        <td scope="row">{{post.body}}</td>
+      </tr>
+
+      </tbody>
+    </table>
+  </div>
+
+</template>
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'PostsExample',
+    data(){
+      return {
+        posts: [
+
+        ]
+      }
+
+    },
+    created(){
+      axios.get('https://jsonplaceholder.typicode.com/posts').then(posts => {
+        this.posts = posts.data
+      });
+    }
+
+  }
+</script>
+<style>
+
+</style>
